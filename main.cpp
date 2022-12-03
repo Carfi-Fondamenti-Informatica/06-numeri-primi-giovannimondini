612cfe5a0ace5fbd4b6744833e132fb5ad1617bd
giovannimondini <117928503+giovannimondini@users.noreply.github.com>
2022-12-03 10:48:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include "lib.h"
 using namespace std;
 int main(){
-  int n=0; cin>>n;
-  int m=(n-1);
-  if (primo (n, m) == true) {
-    cout << "numero primo" ;
-  } else if (primo (n, m) == false) {
-    cout << "numero non primo";
-  }
-  return 0;
-}
+    int a = 0, i = 2;
+    cin >> a;
+    if(numprimo(a, i) == true){
+        cout << "numero primo";
+    }
+    else{
+        cout << "numero non primo";
+    }
+    return 0;}
